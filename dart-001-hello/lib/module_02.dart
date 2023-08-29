074f08c3853999c0cbeeddc221ce2b7830a6bedf
kimgukju <kimgukju@kimgukju.com>
2023-08-29 14:11:02 +0900

    기본구조

---
+++ dart-001-hello/lib/module_02.dart
@@ -0,0 +1,10 @@
+// 위치기반 함수
+String sayHello(String? name, int? age, String? nation) =>
+    "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
+
+String sayHelloNameParams({String? name, int? age, String? nation}) =>
+    "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
+
+// 이름기반 함수
+/// <Span name="홍길동", age=33, nation="대한민국" />
+/// const Span =({name, age, nation}) => { return <></> }
---
+++ dart_project.md
@@ -0,0 +1,9 @@
+# dart project 생성하기
+
+- dart project 의 기본 구조를 생성하는 방법
+
+```bash
+dart create [project]
+dart create dart-001-hello
+cd dart-001-hello
+```
