b6fa43977b5e7bc2f42e2b196d58ac1882004fae
kimgukju <kimgukju@kimgukju.com>
2023-08-30 15:45:14 +0900

    layout

--- dart-001-hello/lib/module_02.dart
+++ dart-001-hello/lib/module_02.dart
@@ -2,7 +2,9 @@
 String sayHello(String? name, int? age, String? nation) =>
     "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
 
-String sayHelloNameParams({String? name, int? age, String? nation}) =>
+/// sayHelloNameParams("대한민국")
+String sayHelloNameParams(String contry,
+        {String? name, int? age, String? nation}) =>
     "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
 
 // 이름기반 함수
