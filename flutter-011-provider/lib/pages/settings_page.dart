f7b420c1df666e2225cc6fb8459f8f4762fd626f
kimgukju <kimgukju@kimgukju.com>
2023-09-13 12:59:36 +0900

    naver_api

--- flutter-011-provider/lib/pages/settings_page.dart
+++ flutter-011-provider/lib/pages/settings_page.dart
@@ -1,41 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:mview/ui_models/page_view_model.dart';
+import 'package:mview/ui_models/theme_controller.dart';
 import 'package:mview/ui_models/timer_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+import '../ui_models/page_view_model.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+  SettingsPage({super.key, required this.themeController});
+  final ThemeController themeController;
+  final textEditController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    var textController = TextEditingController();
-    var setTimer = context
-        .select<TimerViewModel, Function(int)>((value) => value.setTimer);
     var pageViewModel = context.watch<PageViewModel>();
+    var timer = context.watch<TimerViewModel>().timerDto.timer;
+    textEditController.text = timer.toString();
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: textController,
-              decoration: const InputDecoration(
-                hintText: "타이머 입력",
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setTimer(int.parse(textController.text));
-                // 0 번 page 로 되돌아 가기
-                pageViewModel.bottomNavTap(0);
-                // 키보드 감추기
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-              child: const Text("저장"),
-            ),
-          ],
+      appBar: AppBar(
+        title: const Text(
+          "설정",
+          style: TextStyle(color: Colors.white),
         ),
+        backgroundColor: Colors.green,
+        leading: IconButton(
+            color: Colors.white,
+            onPressed: () {
+              pageViewModel.bottomNavTap(0);
+              FocusScope.of(context).unfocus();
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+            )),
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text("타이머 설정"),
+            tiles: [
+              SettingsTile(
+                leading: const Icon(Icons.timer_outlined),
+                title: TextField(
+                  onChanged: (value) {
+                    context.read<TimerViewModel>().setTimer(int.parse(value));
+                  },
+                  controller: textEditController,
+                  decoration: const InputDecoration(labelText: "타이머 시작값"),
+                ),
+              ),
+              SettingsTile(
+                title: const TextField(
+                  decoration: InputDecoration(labelText: "타이머 휴식값"),
+                ),
+                leading: const Icon(Icons.timer_off_outlined),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text("테마 설정"),
+            tiles: [
+              SettingsTile(
+                  title: Row(
+                children: [
+                  const Text("테마모드"),
+                  const SizedBox(width: 20),
+                  DropdownButton<ThemeMode>(
+                    value: themeController.themeMode,
+                    onChanged: (value) =>
+                        themeController.updateThemeMode(value),
+                    items: const [
+                      DropdownMenuItem(
+                          value: ThemeMode.system, child: Text("시스템 테마")),
+                      DropdownMenuItem(
+                          value: ThemeMode.dark, child: Text("어두운 테마")),
+                      DropdownMenuItem(
+                          value: ThemeMode.light, child: Text("밝은 테마")),
+                    ],
+                  )
+                ],
+              )),
+            ],
+          ),
+          SettingsSection(
+            title: const Text("공통 설정"),
+            tiles: [
+              SettingsTile.switchTile(
+                title: const Text("타이머 즉시 실행"),
+                initialValue: false,
+                onToggle: (value) => false,
+              ),
+              SettingsTile(title: const Text("타이머")),
+            ],
+          ),
+        ],
       ),
     );
   }
