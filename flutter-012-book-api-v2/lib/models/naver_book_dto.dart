8e5767fd516c44f9888b805d30a82d54e4496268
kimgukju <kimgukju@kimgukju.com>
2023-09-14 13:04:15 +0900

    도서검색v2

--- flutter-012-book-api-v2/lib/models/naver_book_dto.dart
+++ flutter-012-book-api-v2/lib/models/naver_book_dto.dart
@@ -21,7 +21,11 @@ class NaverBookDto {
     this.pubdate,
   });
 
-  // jsonToDto(jsonToMap)
+  // DtoToMap(jsonToMap)
+  // 데이터를 CU(Create:insert, Update:update)를 해야 하는경우
+  // 주로 NoSQL DB 에 저장할때 Dto 데이터를 Map 형식으로 변환한다
+  // Map 형식으로 변환된 데이터는 JSON type 으로 쉽게 변환이 가능하다
+
   Map<String, dynamic> toMap() {
     final Map<String, dynamic> data = <String, dynamic>{};
 
@@ -38,6 +42,7 @@ class NaverBookDto {
   }
 
   // DtoToJson(FromMap)
+  // openAPI 를 통하여 받은 JSON 데이터를 dto 객체로 변환시키는 method
   NaverBookDto.fromJson(Map<String, dynamic> json) {
     title = json["title"];
     link = json["link"];
